b20ae91750ab556025fb6eb191bc3534480275fd
lorenaboon <lorenaboon@student.rocmondriaan.nl>
2021-02-18 19:51:42 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace hogerlager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welkom bij ons hoger/lager game!");
+            Console.ReadLine();
+            Console.WriteLine("voer uw naam in:");
+            Console.ReadLine();
+            Console.WriteLine("voer uw leeftijd in:");
+            Console.ReadLine();
+            Console.WriteLine("kies een kaart:");
+            Console.ReadLine();
+            Console.WriteLine("kiest u hoger of lager?");
+            Console.ReadLine();
+            Console.WriteLine("gefelicteerd u heeft het goed/helaas u heeft het niet goed");
+            Console.ReadLine();
+            Console.WriteLine("uw geld word met 50% verhoogd/ helaas u bedrag word met 50% afgenomen");
+            Console.ReadLine();
+            Console.WriteLine("wilt u doorgaan?");
+            Console.ReadLine();
+            Console.WriteLine("kies ja of nee om het spel te beindigen of veder te gaan");
+            Console.ReadLine();
+            Console.WriteLine("ja gaat u veder bij nee kies uw bank");
+            Console.ReadLine();
+            Console.WriteLine("u kiest ideal uw geld wat u heeft gewonnen word zo dadelijk op uw rekening gestoort bedankt voor het spelen!");
+            Console.ReadLine();
+        }
+    }
+}
